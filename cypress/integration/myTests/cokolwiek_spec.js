65129a6c37429a3c87e38a354887eb36282b08dd
SzymonNowak <nnja5@o2.pl>
2021-03-29 14:52:11 +0200

    first commit

---
+++ cypress/integration/myTests/cokolwiek_spec.js
@@ -0,0 +1,5 @@
+describe("Moj Testowy Test", () => {
+  it("klikniecie przycisku", () => {
+    cy.visit("https://www.youtube.com/watch?v=MKxE8pCBT7g");
+  });
+});
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,15 @@
+import { HttpService } from './../http.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+})
+export class ListComponent implements OnInit {
+  constructor(private _http: HttpService) {}
+
+  ngOnInit(): void {
+    this._http.myMethod();
+  }
+}
